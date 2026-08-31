14d2a3d5558957c6e1273b6c345ef45ee7d34d61
NosakaN <mayarkhorchid@icloud.com>
2026-08-31 23:19:19 +0100

    Deploy adjustable Sync Pair levels

--- scripts/build_site.py
+++ scripts/build_site.py
@@ -18,18 +18,25 @@ shutil.copytree(upstream, output, ignore=shutil.ignore_patterns(".git", ".github
 (output / "css").mkdir(exist_ok=True)
 shutil.copy2(HERE / "overlay/js/profile-manager.js", output / "js/profile-manager.js")
 shutil.copy2(HERE / "overlay/css/profile-manager.css", output / "css/profile-manager.css")
+shutil.copy2(HERE / "overlay/js/pair-levels.js", output / "js/pair-levels.js")
+shutil.copy2(HERE / "overlay/css/pair-levels.css", output / "css/pair-levels.css")
 
 index = output / "index.html"
 html = index.read_text(encoding="utf-8")
-css_tag = '<link rel="stylesheet" type="text/css" href="css/profile-manager.css">'
-js_tag = '<script type="text/javascript" src="js/profile-manager.js"></script>'
+profile_css_tag = '<link rel="stylesheet" type="text/css" href="css/profile-manager.css">'
+level_css_tag = '<link rel="stylesheet" type="text/css" href="css/pair-levels.css">'
+profile_js_tag = '<script type="text/javascript" src="js/profile-manager.js"></script>'
+level_js_tag = '<script type="text/javascript" src="js/pair-levels.js"></script>'
 css_anchor = '<link rel="stylesheet" type="text/css" id="viewModeCss" href="css/viewmode.css" disabled>'
 js_anchor = '<script type="module" src="js/script.js"></script>'
 
-if css_tag not in html:
+if profile_css_tag not in html:
     if css_anchor not in html:
         raise SystemExit("CSS integration anchor not found")
-    html = html.replace(css_anchor, css_anchor + "\n" + css_tag, 1)
+    html = html.replace(css_anchor, css_anchor + "\n" + profile_css_tag, 1)
+
+if level_css_tag not in html:
+    html = html.replace(profile_css_tag, profile_css_tag + "\n" + level_css_tag, 1)
 
 if '<link rel="manifest" href="manifest.webmanifest">' not in html:
     meta = '\n'.join([
@@ -40,10 +47,13 @@ if '<link rel="manifest" href="manifest.webmanifest">' not in html:
     ])
     html = html.replace("</head>", meta + "\n</head>", 1)
 
-if js_tag not in html:
+if profile_js_tag not in html:
     if js_anchor not in html:
         raise SystemExit("JavaScript integration anchor not found")
-    html = html.replace(js_anchor, js_anchor + "\n" + js_tag, 1)
+    html = html.replace(js_anchor, js_anchor + "\n" + profile_js_tag, 1)
+
+if level_js_tag not in html:
+    html = html.replace(profile_js_tag, profile_js_tag + "\n" + level_js_tag, 1)
 
 index.write_text(html, encoding="utf-8")
 (output / "manifest.webmanifest").write_text('''{
